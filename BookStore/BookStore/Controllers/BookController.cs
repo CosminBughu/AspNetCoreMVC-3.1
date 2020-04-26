f01226facf2a378d38915daaa04cce490a1601ff
Cosmin Ionut <cosminionut7@gmail.com>
2020-04-26 14:48:31 +0300

    Added libman to use client side library

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {   
+        public readonly BookRepository _bookRepository=null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+      public ViewResult GetAllBooks()
+        {
+            var data = _bookRepository.GetAllBooks();
+            return View();
+        }
+
+        public ViewResult GetBook(int id)
+        {
+            var data2 =_bookRepository.GetBookById(id);
+            return View();
+        }
+
+        public ViewResult SearchBook(/*string bookName, string authorName*/)
+        {
+           //var data1= _bookRepository.SearchBook(bookName,authorName);
+            return View();
+        }
+    }
+}
